6b5b7a015048011159ad3d73d5e61091afb4b5cd
Marat <pirat03071988@mail.ru>
2022-09-06 13:32:05 +0200

    INSERT

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Task1
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            Figure[] figures = { new Triangle(8, 10, 15, MessageDialog),
+                                 new Circle(4),
+                                 new Circle(5),
+                                 new Triangle(7, 8, 9, MessageDialog)
+            };
+            foreach (var item in figures)
+            {
+                Console.WriteLine(item.ToString());
+            }
         }
+
+        static void MessageDialog(string message) => Console.WriteLine(message);
     }
 }
