bb7bff4b43c16f231606085c854b8710d58bc01d
Marat <pirat03071988@mail.ru>
2022-09-06 16:28:56 +0200

    UPDATE

--- Task2/Скрипт.sql
+++ Task2/Скрипт.sql
@@ -2,10 +2,10 @@
 /*id - код товара
   ShowcaseCode - витринный код товара
   NameProduct - Название товара*/
-CREATE TABLE Products (IdProduct INT IDENTITY(1,1) NOT NULL, 
+CREATE TABLE Products (ProductId INT IDENTITY(1,1) NOT NULL, 
 			ShowCaseCode INT NOT NULL,
 			NameProduct VARCHAR(50) NOT NULL
-			CONSTRAINT PK_IdProduct PRIMARY KEY (IdProduct),
+			CONSTRAINT PK_ProductId PRIMARY KEY (ProductId ),
 			CONSTRAINT CK_ShowCaseCode CHECK (ShowCaseCode>=0)--Ограничение для витринного кода. Чтобы не было значений меньше либо равно нулю
 )
 --Создаем уникальный некластеризованный индекс для поля ShowcaseCode таблицы Products
@@ -23,21 +23,21 @@ CREATE UNIQUE NONCLUSTERED INDEX IX_NameProduct ON Products --Новое для 
 --Создаем таблицу Категории
 /*IdCategory - код категории
   NameCategories - навазение категории*/
-CREATE TABLE Categories (IdCategory INT IDENTITY(1,1) NOT NULL, 
+CREATE TABLE Categories (CategoryId INT IDENTITY(1,1) NOT NULL, 
 					     NameCategory VARCHAR(50) NOT NULL
-					     CONSTRAINT PK_IdCategories PRIMARY KEY (IdCategory))
+					     CONSTRAINT PK_IdCategories PRIMARY KEY (CategoryId))
 --Чтобы не повторялось название категории, для этого создаем индекс
 CREATE UNIQUE NONCLUSTERED INDEX IX_NameCategories ON Categories --Новое для меня. Создание уникального некластеризованного индекса
 (
 	NameCategory ASC
 )
 --Создаем таблицу Категории
-CREATE TABLE ProductsCategory (IdProduct INT,
-			       IdCategory INT,
-			       CONSTRAINT PK_ProductCategory PRIMARY KEY(IdProduct ASC,
-									 IdCategory ASC),
-CONSTRAINT FK_Products FOREIGN KEY (IdProduct) REFERENCES Products(IdProduct) ON DELETE CASCADE ON UPDATE NO ACTION, --Связываю с таблицей Products
-CONSTRAINT FK_Categories FOREIGN KEY (IdCategory) REFERENCES Categories(IdCategory) ON DELETE CASCADE ON UPDATE NO ACTION --Связываю с таблицей Categories
+CREATE TABLE ProductsCategory (ProductId INT,
+							   CategoryId INT,
+CONSTRAINT PK_ProductCategory PRIMARY KEY(ProductId ASC,
+									      CategoryId ASC),
+CONSTRAINT FK_Products FOREIGN KEY (ProductId) REFERENCES Products(ProductId) ON DELETE CASCADE ON UPDATE NO ACTION, --Связываю с таблицей Products
+CONSTRAINT FK_Categories FOREIGN KEY (CategoryId) REFERENCES Categories(CategoryId) ON DELETE CASCADE ON UPDATE NO ACTION --Связываю с таблицей Categories
 )
 --Заполняею таблицу Products тестовыми данными
 INSERT Products (ShowCaseCode, NameProduct)
@@ -59,13 +59,13 @@ FROM Categories
 SELECT *
 FROM Products
 --Заполняем таблицу ProductsCategory
-INSERT INTO ProductsCategory (IdProduct, IdCategory)
+INSERT INTO ProductsCategory (ProductId, CategoryId)
 VALUES (1,1)
-INSERT INTO ProductsCategory (IdProduct, IdCategory)
+INSERT INTO ProductsCategory (ProductId, CategoryId)
 VALUES (2,1)
-INSERT INTO ProductsCategory (IdProduct, IdCategory)
+INSERT INTO ProductsCategory (ProductId, CategoryId)
 VALUES (3,1)
-INSERT INTO ProductsCategory (IdProduct, IdCategory)
+INSERT INTO ProductsCategory (ProductId, CategoryId)
 VALUES (3,2)
 --Проверка
 SELECT *
@@ -73,6 +73,6 @@ FROM ProductsCategory
 --Запрос
 SELECT p.NameProduct AS [Название продукта], ISNULL(c.NameCategory,'Нет категории') AS Категория
 FROM Products p
-LEFT JOIN ProductsCategory pc ON p.IdProduct=pc.IdProduct
-LEFT JOIN Categories c ON pc.IdCategory=c.IdCategory
+LEFT JOIN ProductsCategory pc ON p.ProductId=pc.ProductId
+LEFT JOIN Categories c ON pc.CategoryId=c.CategoryId
 ORDER BY p.NameProduct
