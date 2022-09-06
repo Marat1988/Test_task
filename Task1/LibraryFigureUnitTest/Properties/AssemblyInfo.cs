fe09861fb32d6b6f5d0b4565dfee213d23966bf4
Marat <pirat03071988@mail.ru>
2022-09-06 20:34:30 +0200

    UPDATE

---
+++ Task1/LibraryFigureUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LibraryFigureTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("LibraryFigureTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a77c369b-ab10-427c-b6e7-6bdf10f259e1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Task1/LibraryFigureUnitTest/TestSquare.cs
@@ -0,0 +1,26 @@
+﻿using LibraryFigure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LibraryFigureTest
+{
+    [TestClass]
+    public class TestSquare
+    {
+        [TestMethod]
+        public void TestRightTriangle() //Тестирование площади прямоугольного треугольника
+        {
+            //Вводные данные
+            int a = 8;
+            int b = 6;
+            int c = 10;
+            double expected = 24; //ожидаю получить 24
+
+            //Создаем треугольник
+            Figure figure1 = new Triangle(a, b, c, null);
+            double actual = figure1.GetSquare();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
